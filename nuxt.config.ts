149da5871488417e36b32995489665fdbfc1aa40
Dmitrii Tkach <dmitrii.tkach@gcore.com>
2025-02-14 15:30:03 +0400

    Revert add startup hook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 import tailwindcss from '@tailwindcss/vite'
-import fetch from 'node-fetch'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
@@ -8,25 +7,6 @@ export default defineNuxtConfig({
     '~/node_modules/@gcorevideo/player-plugins/dist/index.css',
   ],
   devtools: { enabled: true },
-  hooks: {
-    ready: async (nuxt) => {
-      if (import.meta.server && import.meta.env.VERCEL_WEBHOOK_URL) {
-        console.log('ready', typeof nuxt)
-        try {
-          const response = await fetch(import.meta.env.VERCEL_WEBHOOK_URL, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: import.meta.env.VERCEL_WEBHOOK_STARTUP_BODY,
-          })
-          console.log('response', response)
-        } catch (error) {
-          console.error('error', error)
-        }
-      }
-    },
-  },
   modules: ['@pinia/nuxt', '@sentry/nuxt/module'],
   // routeRules: {
   //   '/': {
