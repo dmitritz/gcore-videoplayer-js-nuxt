4ddcfe2b46ad35c59b10eb9e5afda73e0a1ef46f
Dmitrii Tkach <dmitrii.tkach@gcore.com>
2025-04-07 15:44:52 +0400

    Update player (media controls bugfixes)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   css: [
     '~/assets/css/main.css',
-    '~/node_modules/@gcorevideo/player/dist/plugins/index.css',
+    '~/node_modules/@gcorevideo/player/dist/index.css',
   ],
   devtools: { enabled: true },
   modules: ['@pinia/nuxt', '@sentry/nuxt/module'],
